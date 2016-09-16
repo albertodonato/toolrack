8fd7c1e28b3a5ce098cbcab58302a51627626db2
Alberto Donato <alberto.donato@gmail.com>
2016-09-16 17:03:00 +0200

    Split async to a package.

--- toolrack/async/tests/test_periodic.py
+++ toolrack/async/tests/test_periodic.py
@@ -13,8 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ToolRack.  If not, see <http://www.gnu.org/licenses/>.
 
-from ..testing.async import LoopTestCase
-from ..async import PeriodicCall
+from ...testing.async import LoopTestCase
+from ..periodic import PeriodicCall
 
 
 class PeriodicCallTests(LoopTestCase):
